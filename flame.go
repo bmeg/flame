f0edefd429b679cf858e44db1a125607208362a4
Kyle Ellrott <kellrott@gmail.com>
2022-02-23 16:27:57 -0800

    Adding reduce methods

--- flame.go
+++ flame.go
@@ -1,151 +1,196 @@
 package flame
 
 import (
-  "sort"
-  "golang.org/x/exp/constraints"
+	"golang.org/x/exp/constraints"
+	"sort"
 )
 
 type Workflow struct {
-  Nodes []Process
+	Nodes []Process
 }
 
-type KeyValue [K constraints.Ordered, V any] struct {
-  Key K
-  Value V
+type KeyValue[K constraints.Ordered, V any] struct {
+	Key   K
+	Value V
 }
 
-type Node[X,Y any] interface {
-  Init()
-  AddInput (i chan X)
-  GetOutput() chan Y
-  Connect(e Emitter[X])
+type Node[X, Y any] interface {
+	Init()
+	AddInput(i chan X)
+	GetOutput() chan Y
+	Connect(e Emitter[X])
 }
 
 type Emitter[X any] interface {
-  GetOutput() chan X
+	GetOutput() chan X
 }
 
 type Process interface {
-  Init()
+	Init()
 }
 
 func NewWorkflow() *Workflow {
-  return &Workflow{}
+	return &Workflow{}
 }
 
 func (wf *Workflow) Init() {
-  for i := range wf.Nodes {
-    wf.Nodes[i].Init()
-  }
+	for i := range wf.Nodes {
+		wf.Nodes[i].Init()
+	}
 }
 
 /**************************/
 // Mapper
 /**************************/
 
-type MapNode[X,Y any] struct {
-  Input chan X
-  Outputs []chan Y
-  Proc func(X) Y
+type MapNode[X, Y any] struct {
+	Input   chan X
+	Outputs []chan Y
+	Proc    func(X) Y
 }
 
-
-func AddMapper[X,Y any] (w *Workflow, f func(X) Y ) Node[X,Y] {
-  n := &MapNode[X,Y]{Proc: f, Outputs:[]chan Y{}}
-  w.Nodes = append(w.Nodes, n)
-  return n
+func AddMapper[X, Y any](w *Workflow, f func(X) Y) Node[X, Y] {
+	n := &MapNode[X, Y]{Proc: f, Outputs: []chan Y{}}
+	w.Nodes = append(w.Nodes, n)
+	return n
 }
 
-func (n *MapNode[X,Y]) Init() {
-  go func() {
-    for x := range n.Input {
-      y := n.Proc(x)
-      for i := range n.Outputs {
-        n.Outputs[i] <- y
-      }
-    }
-    for i := range n.Outputs {
-      close(n.Outputs[i])
-    }
-  }()
+func (n *MapNode[X, Y]) Init() {
+	go func() {
+		for x := range n.Input {
+			y := n.Proc(x)
+			for i := range n.Outputs {
+				n.Outputs[i] <- y
+			}
+		}
+		for i := range n.Outputs {
+			close(n.Outputs[i])
+		}
+	}()
 }
 
-func (n *MapNode[X,Y]) AddInput (i chan X) {
-  n.Input = i
+func (n *MapNode[X, Y]) AddInput(i chan X) {
+	n.Input = i
 }
 
-func (n *MapNode[X,Y]) GetOutput() chan Y {
-  m := make(chan Y)
-  n.Outputs = append(n.Outputs, m)
-  return m
+func (n *MapNode[X, Y]) GetOutput() chan Y {
+	m := make(chan Y)
+	n.Outputs = append(n.Outputs, m)
+	return m
 }
 
-func (n *MapNode[X,Y]) Connect(e Emitter[X]) {
-  o := e.GetOutput()
-  n.AddInput(o)
+func (n *MapNode[X, Y]) Connect(e Emitter[X]) {
+	o := e.GetOutput()
+	n.AddInput(o)
 }
 
 /**************************/
-// Mapper
+// Sorter
 /**************************/
 
-func AddKeySort[X constraints.Ordered,Y any] (w* Workflow) Node[KeyValue[X,Y],KeyValue[X,Y]] {
-  q := make([]KeyValue[X,Y], 0, 10)
-  n := &SortNode[X,Y]{Queue:q}
-  w.Nodes = append(w.Nodes, n)
-  return n
+func AddKeySort[X constraints.Ordered, Y any](w *Workflow) Node[KeyValue[X, Y], KeyValue[X, Y]] {
+	q := make([]KeyValue[X, Y], 0, 10)
+	n := &SortNode[X, Y]{Queue: q}
+	w.Nodes = append(w.Nodes, n)
+	return n
 }
 
 type SortNode[X constraints.Ordered, Y any] struct {
-  Input chan KeyValue[X,Y]
-  Queue []KeyValue[X,Y]
-  Outputs []chan KeyValue[X,Y]
+	Input   chan KeyValue[X, Y]
+	Queue   []KeyValue[X, Y]
+	Outputs []chan KeyValue[X, Y]
 }
 
 // Swap is part of sort.Interface.
-func (s *SortNode[X,Y]) Swap(i, j int) {
+func (s *SortNode[X, Y]) Swap(i, j int) {
 	s.Queue[i], s.Queue[j] = s.Queue[j], s.Queue[i]
 }
 
 // Less is part of sort.Interface. It is implemented by calling the "by" closure in the sorter.
-func (s *SortNode[X,Y]) Less(i, j int) bool {
+func (s *SortNode[X, Y]) Less(i, j int) bool {
 	return s.Queue[i].Key < s.Queue[j].Key
 }
 
-
 // Len is part of sort.Interface.
-func (s *SortNode[X,Y]) Len() int {
+func (s *SortNode[X, Y]) Len() int {
 	return len(s.Queue)
 }
 
-func (s *SortNode[X,Y]) Init() {
-  go func() {
-    for x := range s.Input {
-      s.Queue = append(s.Queue, x)
-    }    
-    sort.Sort(s)    
-    for _, y := range s.Queue {
-      for i := range s.Outputs {
-        s.Outputs[i] <- y
-      }
-    }
-    for i := range s.Outputs {
-      close(s.Outputs[i])
-    }
-  }()  
+func (s *SortNode[X, Y]) Init() {
+	go func() {
+		for x := range s.Input {
+			s.Queue = append(s.Queue, x)
+		}
+		sort.Sort(s)
+		for _, y := range s.Queue {
+			for i := range s.Outputs {
+				s.Outputs[i] <- y
+			}
+		}
+		for i := range s.Outputs {
+			close(s.Outputs[i])
+		}
+	}()
 }
 
-func (n *SortNode[X,Y]) AddInput (i chan KeyValue[X,Y]) {
-  n.Input = i
+func (n *SortNode[X, Y]) AddInput(i chan KeyValue[X, Y]) {
+	n.Input = i
 }
 
-func (n *SortNode[X,Y]) Connect(e Emitter[KeyValue[X,Y]]) {
-  o := e.GetOutput()
-  n.AddInput(o)
+func (n *SortNode[X, Y]) Connect(e Emitter[KeyValue[X, Y]]) {
+	o := e.GetOutput()
+	n.AddInput(o)
 }
 
-func (n *SortNode[X,Y]) GetOutput() chan KeyValue[X,Y] {
-  m := make(chan KeyValue[X,Y])
-  n.Outputs = append(n.Outputs, m)
-  return m
+func (n *SortNode[X, Y]) GetOutput() chan KeyValue[X, Y] {
+	m := make(chan KeyValue[X, Y])
+	n.Outputs = append(n.Outputs, m)
+	return m
+}
+
+/**************************/
+// Reducer
+/**************************/
+
+type ReduceNode[X, Y any] struct {
+	Start   Y
+	Input   chan X
+	Outputs []chan Y
+	Proc    func(X, Y) Y
+}
+
+func AddReducer[X, Y any](w *Workflow, f func(X, Y) Y, start Y) Node[X, Y] {
+	n := &ReduceNode[X, Y]{Proc: f, Outputs: []chan Y{}, Start: start}
+	w.Nodes = append(w.Nodes, n)
+	return n
+}
+
+func (n *ReduceNode[X, Y]) Init() {
+	go func() {
+		y := n.Start
+		for x := range n.Input {
+			y = n.Proc(x, y)
+		}
+		for i := range n.Outputs {
+			n.Outputs[i] <- y
+		}
+		for i := range n.Outputs {
+			close(n.Outputs[i])
+		}
+	}()
+}
+
+func (n *ReduceNode[X, Y]) AddInput(i chan X) {
+	n.Input = i
+}
+
+func (n *ReduceNode[X, Y]) GetOutput() chan Y {
+	m := make(chan Y)
+	n.Outputs = append(n.Outputs, m)
+	return m
+}
+
+func (n *ReduceNode[X, Y]) Connect(e Emitter[X]) {
+	o := e.GetOutput()
+	n.AddInput(o)
 }
---
+++ tests/reduce_test.go
@@ -0,0 +1,42 @@
+package tests
+
+import (
+	"fmt"
+	"github.com/bmeg/flame"
+	"testing"
+)
+
+func Sum(x, y int) int {
+	return x + y
+}
+
+func TestReduce(t *testing.T) {
+	in := make(chan int, 10)
+
+	wf := flame.NewWorkflow()
+	a := flame.AddReducer(wf, Sum, 0)
+	a.AddInput(in)
+
+	out1 := a.GetOutput()
+	wf.Init()
+
+	v := []int{1, 2, 3, 4, 5}
+	go func() {
+		for _, n := range v {
+			in <- n
+		}
+		close(in)
+	}()
+
+	total := 0
+	for _, j := range v {
+		total += j
+	}
+	for y := range out1 {
+		if y != total {
+			t.Errorf("Incorrect Values %d != %d", y, total)
+		}
+		fmt.Printf("sum: %d\n", y)
+	}
+
+}
