f9d254dc611af0611db5cce55fea52358f728f8f
Kyle Ellrott <kellrott@gmail.com>
2022-03-02 23:49:10 -0800

    Adding flatmap operation

--- flame.go
+++ flame.go
@@ -129,6 +129,51 @@ func (n *MapNode[X, Y]) Connect(e Emitter[X]) {
 }
 
 /**************************/
+// FlatMapper
+/**************************/
+
+type FlatMapNode[X, Y any] struct {
+	Input   chan X
+	Outputs []chan Y
+	Proc    func(X) []Y
+}
+
+func AddFlatMapper[X, Y any](w *Workflow, f func(X) []Y) Node[X, Y] {
+	n := &FlatMapNode[X, Y]{Proc: f, Outputs: []chan Y{}}
+	w.Nodes = append(w.Nodes, n)
+	return n
+}
+
+func (n *FlatMapNode[X, Y]) Start(wf *Workflow) {
+	wf.WaitGroup.Add(1)
+	go func() {
+		for x := range n.Input {
+			y := n.Proc(x)
+			for i := range n.Outputs {
+				for _, z := range y {
+					n.Outputs[i] <- z
+				}
+			}
+		}
+		for i := range n.Outputs {
+			close(n.Outputs[i])
+		}
+		wf.WaitGroup.Done()
+	}()
+}
+
+func (n *FlatMapNode[X, Y]) GetOutput() chan Y {
+	m := make(chan Y)
+	n.Outputs = append(n.Outputs, m)
+	return m
+}
+
+func (n *FlatMapNode[X, Y]) Connect(e Emitter[X]) {
+	o := e.GetOutput()
+	n.Input = o
+}
+
+/**************************/
 // Sorter
 /**************************/
 
---
+++ tests/flatmap_test.go
@@ -0,0 +1,42 @@
+package tests
+
+import (
+  "strings"
+	"github.com/bmeg/flame"
+	"testing"
+)
+
+func Sep(x string) []string {
+  return strings.Split(x, "")
+}
+
+func TestFlatMap(t *testing.T) {
+	in := make(chan string, 10)
+
+	wf := flame.NewWorkflow()
+	inc := flame.AddSourceChan(wf, in)
+	a := flame.AddFlatMapper(wf, Sep)
+	a.Connect(inc)
+
+	out1 := a.GetOutput()
+	wf.Start()
+
+	v := []string{"hello", "world"}
+	go func() {
+		for _, n := range v {
+			in <- n
+		}
+		close(in)
+	}()
+
+  count := 0
+	for y := range out1 {
+    if len(y) != 1 {
+      t.Errorf("Incorrect length output")      
+    }
+    count += 1
+	}
+  if count != len(v[0]) + len(v[1]) {
+    t.Errorf("Incorrect output count")
+  }
+}
